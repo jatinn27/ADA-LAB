42e55c1342c6ef55ad6fd7b03c7eb92415b88e6e
jatinn27 <jatin121dhiman@gmail.com>
2024-01-19 10:13:06 +0530

    Added git ignore

--- SelectionSortAnalysis.cpp
+++ SelectionSortAnalysis.cpp
@@ -1,26 +1,27 @@
 #include <iostream>
-#include <time.h>
 #include <vector>
-#include <iomanip>
-#include <cstdlib> 
+#include <iomanip> // setprecision()
+#include <cstdlib> // clock_t
 using namespace std;
 
 vector<int> randomnumbers(vector<int> arr, int n)
 {
-    int N=100;
+    int N=100;      // This function will generate random numbers for n=50 it will generate 50 numbers
     for (int i = 0; i < n; i++){
-        arr.push_back(rand() % N);
+        arr.push_back(rand() % N); 
     }
 
     return arr;
 }
 
 void print(vector<int>arr){
+    // to print the numbers generated 
     for(int i=0;i<arr.size();i++){
         cout<<arr[i]<<" ";
     }
 }
 
+// Function for Selection sort 
 void SelectionSort(vector<int> &arr)
 {
     int min;
@@ -44,11 +45,12 @@ void SelectionSort(vector<int> &arr)
 
 int main()
 {
-    vector<int> arr;
+    vector<int> arr; // creating a dynamic array
 
+    //generating random numbers
     arr=randomnumbers(arr,500);
 
-
+    // to calculate time if an algorithm runs 100 times for a specific input 
     clock_t start, end;
 
     start = clock();
@@ -57,8 +59,11 @@ int main()
     SelectionSort(arr);
 
     end = clock();
+    // total time taken for running an algorithm 100 times for a specific input  
 	double time_taken = double(end - start)/CLOCKS_PER_SEC;
+    //avearge time time_taken/100
     double average_time=time_taken/100;
+
 	cout << "\nAverage Time taken by program is : " << fixed 
 		<< time_taken << setprecision(5);
 	cout << " sec " << endl;
