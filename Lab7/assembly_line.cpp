b089d27587d881f93f3dd7c2c1f57ed2f6c7adcf
jatinn27 <jatin121dhiman@gmail.com>
2024-04-18 09:37:01 +0530

    .

--- Lab7/assembly_line.cpp
+++ Lab7/assembly_line.cpp
@@ -1,43 +1,30 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int FASTESTWAY(int a[][4], int t[][4], int e[], int x[]){
-    int f1[4];
-    int f2[4];
-    
-    f1[1]=e[1]+a[1][1];
-    f2[1]=e[2]+a[2][1];
-    
-    for(int j=2; j<4; j++){
-        if(f1[j-1]+a[1][j]<=f2[j-1]+t[2][j-1]+a[1][j]){
-            return f1[j]=f1[j-1]+a[1][j];
-        }
-            else
-                f1[j]=f2[j-1]+t[2][j-1]+a[1][j];
-        if(f2[j-1]+a[2][j]<=f1[j-1]+t[1][j-1]+a[2][j]){
-            return f2[j]=f2[j-1]+a[2][j];
-        }
-            else
-                f2[j]=f1[j-1]+t[1][j-1]+a[2][j];
-            
-        if(f1[4]+x[1]<=f2[4]+x[2]){
-             return f1[0]=f1[4]+x[1];
-        }
-        
-            else
-                return f2[0]=f2[4]+x[2];
-        
+int FASTESTWAY(vector<vector<int>>& a, vector<vector<int>>& t, vector<int>& e, vector<int>& x) {
+    int n = a[0].size();
+    vector<int> f1(n);
+    vector<int> f2(n);
+
+    f1[0] = e[0] + a[0][0];
+    f2[0] = e[1] + a[1][0];
+
+    for (int j = 1; j < n; j++) {
+        f1[j] = min(f1[j - 1] + a[0][j], f2[j - 1] + t[1][j - 1] + a[0][j]);
+        f2[j] = min(f2[j - 1] + a[1][j], f1[j - 1] + t[0][j - 1] + a[1][j]);
     }
-    
+
+    return min(f1[n - 1] + x[0], f2[n - 1] + x[1]);
 }
 
-int main(){
-    int a[][4] = {{1, 2, 3, 4},  {5, 6, 7, 8}};  
-    int t[][4] = {{0, 4, 3, 5},   {0, 6, 2, 9}};  
-    int e[] = {14, 13};
-    int x[] = {14, 5}; 
+int main() {
+    vector<vector<int>> a = {{1, 2, 3, 4}, {5, 6, 7, 8}};  
+    vector<vector<int>> t = {{0, 4, 3, 5}, {0, 6, 2, 9}};  
+    vector<int> e = {14, 13};
+    vector<int> x = {14, 5}; 
     
-    cout<<FASTESTWAY(a,t,e,x);
+    cout << FASTESTWAY(a, t, e, x);
     
     return 0;
 }
